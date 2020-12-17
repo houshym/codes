d9224826be66582e7edcce77d17f86d89e2cb719
mohsen <houshym@mail.com>
2020-12-17 11:58:20 -0800

    test

---
+++ credit_card.py
@@ -0,0 +1,6 @@
+# this program is read credit card transaction and calculate resposne time and send alert after specified tereshold
+print("let's see what is going on in transactions")
+card_list = []
+for i in range(100):
+    card_list.append(i)
+print(card_list)
---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "local_file" "credit_card" {
+  filename = "~/credit_card.txt"
+  content  = "this is for test"
+
+}
